cffb60af43962e8849ba2a8a30ad4bf9b9a88074
sparks <spmutugi@gmail.com>
2024-01-30 23:03:45 +0300

    rectangle with height and width constructors

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./0-rectangle');
-
-const r1 = new Rectangle(a, b);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+class Rectangle{
+    constructor(w,h){
+        this.width = w;
+        this.height = h;
+    }
+}
+module.exports = Rectangle;
