018c6e0ab1cd676d70802aedb023dede2141484d
sparks <spmutugi@gmail.com>
2024-01-31 01:02:09 +0300

    square

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 const square1 = require('./5-square')
 class Square extends square1 {
     charPrint(c) {
-       const char = c || 'x';
+       const char = c || 'X';
 
         // Print the square using the specified character
         for (let i = 0; i < this.height; i++) {
